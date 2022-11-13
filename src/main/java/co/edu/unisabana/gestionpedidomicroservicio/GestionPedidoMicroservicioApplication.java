9a8f163353b0c5481fd64e2bc34a9077ac1e3588
rximenars <ximis.ximena@gmail,com>
2022-11-13 00:05:42 -0500

    Spring del proyecto, gestión de pedidos base

---
+++ src/main/java/co/edu/unisabana/gestionpedidomicroservicio/GestionPedidoMicroservicioApplication.java
@@ -0,0 +1,13 @@
+package co.edu.unisabana.gestionpedidomicroservicio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionPedidoMicroservicioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionPedidoMicroservicioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/unisabana/gestionpedidomicroservicio/GestionPedidoMicroservicioApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.unisabana.gestionpedidomicroservicio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionPedidoMicroservicioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
